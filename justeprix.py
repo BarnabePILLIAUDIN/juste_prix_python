cc27994bfb0d6eb7bdbc1a634f7b3dd34435a037
BarnabePILLIAUDIN <barnabe.pilliaudin@gmail.com>
2022-11-14 11:23:18 +0100

    renomage de certaines variables

--- justeprix.py
+++ justeprix.py
@@ -4,35 +4,35 @@ print("Bienvenue dans le juste prix")
 play = True
  
 def game():
-    winningNumber = randint(1, 100)
-    userNumber = int(input("Entrez un nombre \n"))
+    winning_number = randint(1, 100)
+    user_number = int(input("Entrez un nombre \n"))
     lives = 4
-    lastbigger = 100
-    lastless = 1
+    last_bigger= 100
+    last_less = 1
 
-    while userNumber != winningNumber and lives > 0:
+    while user_number != winning_number and lives > 0:
         lives -= 1
-        if userNumber > winningNumber:
+        if user_number > winning_number:
             print("c'est moins.")
-            if userNumber< lastbigger:
-                lastbigger = userNumber
-        elif userNumber < winningNumber:
+            if user_number< last_bigger:
+             last_bigger= user_number
+        elif user_number < winning_number:
             print("c'est plus")
-            if userNumber> lastless:
-                lastless = userNumber
+            if user_number> last_less:
+                last_less = user_number
         print("il vous reste " + str(lives+1))
-        userNumber = int(
-            input("Entrez un nombre entre " + str(lastless) + " et "+str(lastbigger) + "\n"))
+        user_number = int(
+            input("Entrez un nombre entre " + str(last_less) + " et "+str(last_bigger) + "\n"))
 
     if lives == 0:
         print("Vous avez perdus pas de chances")
-        print("Le juste prix était " + str(winningNumber))
+        print("Le juste prix était " + str(winning_number))
     else:
         print("Ceci est le jsute prix bien joué")
-    choix = input("Voulez vous rejouer? o/n \n")
-    if choix == "o":
+    choice= input("Voulez vous rejouer? o/n \n")
+    if choice== "o":
         return True
-    elif choix == "n":
+    elif choice== "n":
         return False
     else:
         print("Entrée invalide le programe va s'éteindre.")
